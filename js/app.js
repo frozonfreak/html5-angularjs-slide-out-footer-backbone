3ccde8f3bb640ad950765cf7ebb35d3894ac8cca
Sastha K L <shazcool@gmail.com>
2013-10-15 20:34:50 +0800

    initial commit

---
+++ js/app.js
@@ -0,0 +1,36 @@
+var smApp = angular.module('smApp',['ui.bootstrap','ui.router']);
+
+smApp.config(function($stateProvider, $urlRouterProvider) {
+
+  // Now set up the states
+  $stateProvider
+    .state('home', {
+      url: "/",
+      templateUrl: "partials/home.html",
+      controller: 'appController',
+    })
+    // For any unmatched url, redirect to /state1
+     $urlRouterProvider.otherwise("/404");
+
+});
+//controller
+smApp.controller('appController', function($scope){
+
+	$scope.taskList = [];
+	$scope.task;
+	$scope.taskDetail;
+	$scope.deadLine;
+	$scope.alerts = [];
+	$scope.searchText;
+	
+  	//Initializer
+	init();
+	function init(){
+		
+		
+	};
+	$scope.isActive = function (viewLocation) { 
+        return viewLocation === $location.path();
+    };
+	
+});
